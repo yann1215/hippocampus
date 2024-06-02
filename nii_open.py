9d5f4a58ffb469fd5d630a1357c80208046fd544
Yann <yanshen0117@outlook.com>
2024-06-02 11:59:23 +0800

    2024/6/2 11:59 master nii_open.py

--- nii_open.py
+++ nii_open.py
@@ -5,25 +5,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# def explore_3dimage(layer):
-#     plt.figure(figsize=(10, 5))
-#     plt.imshow(image_data[:, :, layer], cmap='gray');
-#     plt.title('Explore Layers of adrenal', fontsize=20)
-#     plt.axis('off')
-#     return layer
-
-
 sns.set_style("darkgrid")
 
 image_path = "data_preprocessed/preprocess_second/s001_second_preprocessed.nii"
-# mask_path = "D:\0_ZJU\图像dip\大作业\data_preprocessed\mask_second\s001_mask_second"
 
 image_obj = nib.load(image_path)
 # print(f"Type of the image {type(image_obj)}")
 
-
 image_data = image_obj.get_fdata()
 # type(image_data)
+
+print("max value of an image", np.max(image_data))
 
 height, width, depth = image_data.shape
 print(f"The image object height: {height}, width:{width}, depth:{depth}")
@@ -33,18 +25,13 @@ print(f"image value range: [{image_data.min()}, {image_data.max()}]")
 
 # print(image_obj.header.keys())
 pixdim = image_obj.header["pixdim"]
-print(f"z轴分辨率： {pixdim[3]}")
-print(f"in plane 分辨率： {pixdim[1]} * {pixdim[2]}")
+print(f"z-axis resolution ratio： {pixdim[3]}")
+print(f"in plane resolution ratio： {pixdim[1]} * {pixdim[2]}")
 
 z_range = pixdim[3] * depth
 x_range = pixdim[1] * height
 y_range = pixdim[2] * width
 print(x_range, y_range, z_range)
-
-# 随机显示某一层
-# maxval = 177
-# i = np.random.randint(0, maxval)
-# Define a channel to look at
 
 i = 50
 print(f"Plotting z Layer {i} of Image")
